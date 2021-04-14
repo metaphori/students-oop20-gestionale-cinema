9881c379b05cd43827bac060254d8976b9f41355
AlessandroZirondelli <alessandr.zirondelli@studio.unibo.it>
2021-04-14 22:51:54 +0200

    Add ProgrammingController and ProgrammedFilmsModel first implementation

--- src/main/java/model/ProgrammedFilmsModel.java
+++ src/main/java/model/ProgrammedFilmsModel.java
@@ -1,5 +1,16 @@
 package model;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.*;
 
+import utilities.Film;
+import utilities.ProgrammedFilm;
 public interface ProgrammedFilmsModel {
-
+    
+    public Collection<ProgrammedFilm> getProgrammedFilms();
+    public Film getSelectedFilm(String film, LocalDate date, LocalTime startTime, int hallNumber); //return 
+    public void deleteSelectedProgrammation();
+    
+    
+    
 }
