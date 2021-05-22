77126a72068e896eddbf417e46eb00503c6924fd
Lorenzo Sansone <lorenzo.sansone3@studio.unibo.it>
2021-05-22 14:28:37 +0200

    update bookingControllerImpl

--- src/main/java/application/Cinema.java
+++ src/main/java/application/Cinema.java
@@ -1,12 +1,12 @@
 package application;
 
-
+import controllerImpl.BookingControllerImpl;
 
 public class Cinema {
 
 	public static void main(String[] args) {
 	  	  
-	     
+	     new BookingControllerImpl().test();
 	}
 
 
