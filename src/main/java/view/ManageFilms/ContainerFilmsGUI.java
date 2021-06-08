bab24b3e7ab89ac1b39b1e0ce37d067685e788a4
AlessandroZirondelli <alessandr.zirondelli@studio.unibo.it>
2021-06-08 23:32:44 +0200

    Add documentation in ManagerWorkingDir and ContainerFilmsGUI

--- src/main/java/view/ManageFilms/ContainerFilmsGUI.java
+++ src/main/java/view/ManageFilms/ContainerFilmsGUI.java
@@ -3,7 +3,17 @@ package view.ManageFilms;
 import controller.ManageFilms.FilmsController;
 
 public interface ContainerFilmsGUI {
+    /**
+        Show a view with all films added in container.
+     */
     void start();
+    /**
+        Set GUI observer.
+        @param observer observer to set.
+     */
     void setObserver(FilmsController observer);
+    /**
+        Update and refresh GUI.
+     */
     void update();
 }
--- src/main/java/view/ManageFilms/Factory/ContainerFilmsGUIfactory.java
+++ src/main/java/view/ManageFilms/Factory/ContainerFilmsGUIfactory.java
@@ -5,10 +5,25 @@ import java.awt.LayoutManager;
 import javax.swing.Icon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+/**
+    Factory to create components for container films GUI.
+*/
 
 public interface ContainerFilmsGUIfactory {
+    /**
+        Creates personalized panel using specific layout manager.
+        @param layout layout to used.
+     */
     JPanel createPanel(LayoutManager layout);
+    /**
+        Creates personalized button using with specific text.
+        @param text text to used.
+     */
     JButton createButtonWithText(String text);
+    /**
+        Creates personalized button with icon.
+        @param icon icon to used.
+     */
     JButton createButtonWithIcon(Icon icon);
 
 }
