eac98e6c320b5df5960fa81afa41866b501c8994
AlessandroZirondelli <alessandr.zirondelli@studio.unibo.it>
2021-06-14 23:10:25 +0200

    Setting proportion with view settings in containerfilmsgui

--- src/main/java/utilitiesImpl/ViewSettings.java
+++ src/main/java/utilitiesImpl/ViewSettings.java
@@ -5,9 +5,10 @@ import java.awt.Toolkit;
 public final class ViewSettings {
     public static final double DIMENSION_HEIGTH_SCREEN = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
     public static final double DIMENSION_WIDTH_SCREEN = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
-    
-    public static final double DIMENSION_HEIGTH_VIEW = DIMENSION_HEIGTH_SCREEN * 0.5;
-    public static final double DIMENSION_WIDTH_VIEW = DIMENSION_WIDTH_SCREEN * 0.5;
+
+    public static final double PROPORTION = 1.15;
+    public static final double DIMENSION_HEIGTH_VIEW = DIMENSION_HEIGTH_SCREEN / PROPORTION ;
+    public static final double DIMENSION_WIDTH_VIEW = DIMENSION_WIDTH_SCREEN / PROPORTION;
     private ViewSettings() {};
     
     
