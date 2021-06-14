f37d2d70222715e86f6a721fd38bcb59ada0208c
AlessandroZirondelli <alessandr.zirondelli@studio.unibo.it>
2021-06-14 22:19:20 +0200

    Set center location of menu

--- src/main/java/viewImpl/ManageMenu/MenuViewImpl.java
+++ src/main/java/viewImpl/ManageMenu/MenuViewImpl.java
@@ -16,16 +16,16 @@ import utilities.ManageAccounts.TypeAccount;
 import utilitiesImpl.ManageAccounts.LoggedAccount;
 import view.ManageMenu.MenuView;
 
-public class MenuViewImpl implements MenuView {
-    CinemaControllerObserver observer;
-    JFrame frame = new JFrame();
-    
+public final class MenuViewImpl implements MenuView {
+    private CinemaControllerObserver observer;
+    private final JFrame frame = new JFrame();
+
     private static final String BTN_CONTROLLER_ACCOUNT_TITLE = "MANAGE ACCOUNT";
     private static final String BTN_CONTROLLER_PROGRAMMED_FILM_TITLE = "MANAGE PROGRAMMED FILM";
     private static final String BTN_CONTROLLER_FILM_TITLE = "MANAGE FILM";
     private static final String BTN_CONTROLLER_TICKET_TITLE  = "MANAGE TICKET";
     private static final String BTN_CONTROLLER_STATISTICS_TITLE = "MANAGE STATISTICS";
-    
+
     private static final double WIDTH_PERC_FRAME = 0.5;
     private static final double HEIGTH_PERC_FRAME = 0.5;
     private static final double WIDTH_MINIMUM_FRAME = WIDTH_PERC_FRAME / 0.7;
@@ -37,7 +37,7 @@ public class MenuViewImpl implements MenuView {
         final JButton btnControllerProgrammingFilms = new JButton(BTN_CONTROLLER_PROGRAMMED_FILM_TITLE);
         final JButton btnControllerStatistics = new JButton(BTN_CONTROLLER_STATISTICS_TITLE);
         final JButton btnControllerTicket = new JButton(BTN_CONTROLLER_TICKET_TITLE);
-        
+
         final JPanel mainPanel = new JPanel(new BorderLayout());
         final JPanel centerPanel = new JPanel(new FlowLayout());
         centerPanel.add(btnControllerAccount);
@@ -45,56 +45,46 @@ public class MenuViewImpl implements MenuView {
         centerPanel.add(btnControllerProgrammingFilms);
         centerPanel.add(btnControllerStatistics);
         centerPanel.add(btnControllerTicket);
-        
-        btnControllerAccount.addActionListener( a -> {
+
+        btnControllerAccount.addActionListener(a -> {
             this.observer.showControllerAccount();
             frame.dispose();
         });
-        btnControllerProgrammingFilms.addActionListener( a -> {
+        btnControllerProgrammingFilms.addActionListener(a -> {
             this.observer.showControllerProgrammingFilms();
             frame.dispose();
         });
-        btnControllerTicket.addActionListener( a -> {
+        btnControllerTicket.addActionListener(a -> {
             this.observer.showControllerTicket();
             frame.dispose();
         });
-        btnControllerFilm.addActionListener( a -> {
+        btnControllerFilm.addActionListener(a -> {
             this.observer.showControllerFilm();
             frame.dispose();
         });
-        btnControllerStatistics.addActionListener( a -> {
+        btnControllerStatistics.addActionListener(a -> {
             this.observer.showControllerStatistics();
             frame.dispose();
         });
-        LoggedAccount log = LoggedAccount.getIstance();
-        Account accountLogged = log.getAccount();
-        if(accountLogged.isAdmin().equals(TypeAccount.OPERATOR)) {
+        final LoggedAccount log = LoggedAccount.getIstance();
+        final Account accountLogged = log.getAccount();
+        if (accountLogged.isAdmin().equals(TypeAccount.OPERATOR)) {
             btnControllerAccount.setEnabled(false);
-        }
-        if(accountLogged.isAdmin().equals(TypeAccount.OPERATOR)) {
             btnControllerProgrammingFilms.setEnabled(false);
-        }
-        if(accountLogged.isAdmin().equals(TypeAccount.OPERATOR)) {
             btnControllerFilm.setEnabled(false);
-        }
-        if(accountLogged.isAdmin().equals(TypeAccount.OPERATOR)) {
             btnControllerStatistics.setEnabled(false);
         }
         mainPanel.add(centerPanel, BorderLayout.CENTER);
-        
-        
         final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         this.frame.setMinimumSize(new Dimension((int) (screenSize.getWidth() * WIDTH_MINIMUM_FRAME), (int) (screenSize.getHeight() * HEIGTH_MINMUM_FRAME)));
 
        frame.getContentPane().add(mainPanel);
-        
+       frame.setLocationByPlatform(true);
     }
-    public void setObserver(final 
-            CinemaControllerObserver observer) {
+    public void setObserver(final CinemaControllerObserver observer) {
         this.observer = observer;
     }
     public void show() {
         frame.setVisible(true);
     }
-  
 }
