9d2455d1075319f7985a9fc7bd6492c47e933ed2
Arianna Pagano <arianna.pagano@studio.unibo.it>
2021-06-14 23:07:14 +0200

    code style improvement

--- src/main/java/viewImpl/ManageAccounts/ManagementAccountImplGUI.java
+++ src/main/java/viewImpl/ManageAccounts/ManagementAccountImplGUI.java
@@ -39,13 +39,13 @@ public class ManagementAccountImplGUI implements ManagementAccountGUI{
     private static final String FRAME_NAME = "Management account";
     private static final double PROPORTION = 1.15;
     private final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-    final JFrame frame = new JFrame();
+    private final JFrame frame = new JFrame();
     
     //components
-    final JLabel title = new JLabel("List account"); 
-    final JButton add = new JButton("Add");
-    final JButton home = new JButton("Home");
-    final JButton delete = new JButton("Delete");
+    private final JLabel title = new JLabel("List account"); 
+    private final JButton add = new JButton("Add");
+    private final JButton home = new JButton("Home");
+    private final JButton delete = new JButton("Delete");
     private JTable table = new JTable();
     
     //real dimension of the screen
@@ -54,26 +54,26 @@ public class ManagementAccountImplGUI implements ManagementAccountGUI{
     //real dimension of my frame
     private final int frameWidth = (int) (screenWidth / PROPORTION);
     private final int frameHeight = (int) (screenHeight / PROPORTION);
-    
+
     private AccountsController observer;
     public static final int SPACE = 5;
 
     private Map<JButton, Account> map = new HashMap<>();
-    
-    public ManagementAccountImplGUI () {
-        
+
+    public ManagementAccountImplGUI() {
+
         frame.setTitle(FRAME_NAME);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    
-        final GridBagConstraints cnst = new GridBagConstraints ();
+
+        final GridBagConstraints cnst = new GridBagConstraints();
         cnst.gridy = 0;
-        cnst.insets = new Insets (SPACE, SPACE, SPACE, SPACE); 
+        cnst.insets = new Insets(SPACE, SPACE, SPACE, SPACE); 
         cnst.fill = GridBagConstraints.HORIZONTAL;
-        
-        final JPanel pNorth = new JPanel (new FlowLayout ());
+
+        final JPanel pNorth = new JPanel(new FlowLayout ());
         pNorth.add(title);
-        cnst.gridy ++;
-        
+        cnst.gridy++;
+
         DefaultTableModel dm = new DefaultTableModel(new Object[][] {},new Object[] {"Username", "Name", "Surname", "Type"});
         table = new JTable(dm) {
             private static final long serialVersionUID = 1L;
@@ -82,72 +82,69 @@ public class ManagementAccountImplGUI implements ManagementAccountGUI{
                 }
             };
         table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        
-        final JScrollPane scroll = new JScrollPane (table); //visione scorrevole del componente
-        scroll.setVerticalScrollBarPolicy ( ScrollPaneConstants .   VERTICAL_SCROLLBAR_AS_NEEDED );
-        final JPanel pWestInternal = new JPanel ( new GridBagLayout ()); // Griglia flessibile
+
+        final JScrollPane scroll = new JScrollPane(table); //visione scorrevole del componente
+        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+        final JPanel pWestInternal = new JPanel(new GridBagLayout()); // Griglia flessibile
         pWestInternal.add(scroll);
-        
+
         final JPanel pEst = new JPanel();
         pEst.add(home);
-        
-        final JPanel pSouth = new JPanel (new FlowLayout (FlowLayout.CENTER));
+
+        final JPanel pSouth = new JPanel(new FlowLayout(FlowLayout.CENTER));
         pSouth.add(add);
         pSouth.add(delete);
-        
-        frame.add (pNorth, BorderLayout.NORTH);
-        frame.add (pWestInternal, BorderLayout.CENTER);
+
+        frame.add(pNorth, BorderLayout.NORTH);
+        frame.add(pWestInternal, BorderLayout.CENTER);
         frame.add(pSouth, BorderLayout.SOUTH);
         frame.add(pEst, BorderLayout.EAST);
-        
-       
+
         //method to registration view 
         add.addActionListener(event -> {
             observer.showRegistrationAccountView(); 
             frame.setVisible(false);
         });
-        
-     
+
         home.addActionListener(event -> {
             observer.showMenu();
             frame.setVisible(false);
         });
-        
+
         delete.addActionListener(event -> {
             final int row = table.getSelectedRow();
             if (row != -1) {
             final String username = (String) table.getModel().getValueAt(row, 0);
-            
+
             Account account = observer.getAccounts().stream()
             .filter(a -> a.getUsername().equals(username))
             .findFirst().get();
-            observer.deleteAccount (account);
+            observer.deleteAccount(account);
             this.update();
             } else {
-                JOptionPane.showMessageDialog(frame, "No row selected" );
+                JOptionPane.showMessageDialog(frame, "No row selected");
             }
         });
-        
+
         frame.setMinimumSize(new Dimension(frameWidth, frameHeight));
         frame.validate();
-     
     }
-     
+
     @Override
-    public void show () {
+    public void show() {
         frame.pack();
         frame.setLocationByPlatform(true);
         frame.setVisible(true);
-        if(observer.getAccounts().isEmpty()) {
+        if (observer.getAccounts().isEmpty()) {
             this.showDialog();
         }
     }
-    
+
     @Override
     public void setObserver(final AccountsController observer) {
         this.observer = observer;
     }
-    
+
     //update table of list account
     @Override
     public void update() {
@@ -157,22 +154,20 @@ public class ManagementAccountImplGUI implements ManagementAccountGUI{
         Object[][] data = new Object[row][columnNames.length];
         int i = 0;
         for (final var acc : setAccount) {
-        data[i][0] = acc.getUsername();
-        data[i][1] = acc.getName();
-        data[i][2] = acc.getSurname();
-        data[i][3] = acc.isAdmin();
-        i++;
+            data[i][0] = acc.getUsername();
+            data[i][1] = acc.getName();
+            data[i][2] = acc.getSurname();
+            data[i][3] = acc.isAdmin();
+            i++;
         }
-         
-         final DefaultTableModel model = (DefaultTableModel) table.getModel();
-         table.setModel(new  DefaultTableModel(data, columnNames));
-         model.fireTableDataChanged();
-         
-         frame.validate();
+
+        final DefaultTableModel model = (DefaultTableModel) table.getModel();
+        table.setModel(new  DefaultTableModel(data, columnNames));
+        model.fireTableDataChanged();
+        frame.validate();
     }
-    
+
     private void showDialog() {
         JOptionPane.showMessageDialog(frame, "Please add your personal information so as to avoid using default username and password." );
     }
-    
 }
