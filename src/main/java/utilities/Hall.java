beb616141b8cbbaa4459770c50e48ebf90440986
AlessandroZirondelli <alessandr.zirondelli@studio.unibo.it>
2021-06-16 12:55:10 +0200

    Add Hall enum

---
+++ src/main/java/utilities/Hall.java
@@ -0,0 +1,11 @@
+package utilities;
+/**
+ * Describes a hall in the cinema.
+ *  */
+public enum Hall {
+    /**
+     * Hall names.
+     *  */
+    HALL_1, HALL_2, HALL_3, HALL_4, HALL_5
+
+}
--- src/main/java/utilitiesImpl/Hall.java
+++
@@ -1,5 +0,0 @@
-package utilitiesImpl;
-
-public class Hall {
-
-}
